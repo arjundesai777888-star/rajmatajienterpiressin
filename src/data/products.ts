2d1fb8534a31eb7d01b534c28042b72386dd9748
arjundesai777888-star <arjundesai777888@gmail.com>
2026-06-21 23:36:47 +0530

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -8,14 +8,15 @@ export interface Product {
 }
 
 const products: Array<Product> = [
-  {
-    id: 1,
-    name: 'Product 1',
-    image: '/placeholder.png',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    shortDescription: 'A generic product description for your first product.',
-    price: 3000,
+  
+  id: 1,
+  name: 'Jolly Cover',
+  image: '/product2 (1).jpg',
+  description: 'Jolly Cover',
+  shortDescription: 'Jolly Cover',
+  price: 160,
+},
+    price: 160
   },
 ]
 
